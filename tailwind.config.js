8c1c3f55b7f4f6a30afd0c0ff8f662a699e95c74
vatsalsaglani <saglanivatsal@gmail.com>
2022-04-27 21:36:55 +0530

    tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: "#14141c",
+      },
+      animation: {
+        tilt: "tilt 2s infinite linear",
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(0.5deg)",
+          },
+          "75%": {
+            transform: "rotate(-0.5deg)",
+          },
+        },
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      fontFamily: {
+        Work: ["Work Sans", "sans-serif"],
+        Bebas: ["Bebas Neue", "cursive"],
+        Dancing: ["Dancing Script", "cursive"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
