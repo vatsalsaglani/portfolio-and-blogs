a70725cf64d869c87cc0ec740b2dd7c5fb4d6022
vatsalsaglani <saglanivatsal@gmail.com>
2022-04-27 22:20:14 +0530

    components for markdown file

---
+++ components/MDXComponents.js
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+export const CodeBlock = (props) => {
+  // console.log("CODE PROPS: ", props);
+
+  return (
+    <div>
+      <pre {...props} />
+    </div>
+  );
+};
+
+export const CustomLink = (props) => {
+  const href = props.href;
+  const isInternal = href && (href.startsWith("/") || href.startsWith("#"));
+  if (isInternal) {
+    return (
+      <Link href={href}>
+        <a className="text-sky-500 hover:text-sky-800" {...props}>
+          {props.children}
+        </a>
+      </Link>
+    );
+  }
+
+  return (
+    <a
+      className="text-sky-500 hover:text-sky-800"
+      target={"_blank"}
+      rel={"noopener noreferrer"}
+      {...props}
+    />
+  );
+};
+
+export const RoundedImage = (props) => {
+  return (
+    // <div className="items-center">
+    <Image alt={props.alt} className={"rounded-lg shadow-lg"} {...props} />
+    // </div>
+  );
+};
+
+export const InlineCode = (props) => {
+  return <span {...props} className={`p-1 bg-yellow-200/70 rounded-md`} />;
+};
+
+const MDXComponents = {
+  Image: RoundedImage,
+  a: CustomLink,
+  pre: CodeBlock,
+  InlineCode,
+};
+
+export default MDXComponents;
