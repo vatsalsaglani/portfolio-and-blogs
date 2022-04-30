9d7a3b5feba3a8fdabc25ba3c143d89be3350e50
vatsalsaglani <saglanivatsal@gmail.com>
2022-04-30 15:12:14 +0530

    leetcode document type

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -6,6 +6,59 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrism from "rehype-prism-plus";
 import readingTime from "reading-time";
 
+export const Leetcode = defineDocumentType(() => ({
+  name: "Leetcode",
+  filePathPattern: "leetcode/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of Leetcode question",
+      required: true,
+    },
+    date: {
+      type: "date",
+      description: "The date of the post",
+      required: true,
+    },
+    question: {
+      type: "string",
+      description: "Leetcode question",
+      required: true,
+    },
+    questionLink: {
+      type: "string",
+      description: "Link to Leetcode question",
+      required: true,
+    },
+    thumbnail: {
+      type: "string",
+      description: "Link to Thumbnail Image",
+      required: true,
+    },
+    difficulty: {
+      type: "string",
+      description: "Difficulty level of the question",
+      required: true,
+    },
+  },
+  computedFields: {
+    readingTime: { type: "json", resolve: (doc) => readingTime(doc.body.raw) },
+    wordCount: {
+      type: "number",
+      resolve: (doc) => doc.body.raw.split(/\s+/gu).length,
+    },
+    url: {
+      type: "string",
+      resolve: (post) => `/${post._raw.flattenedPath}`,
+    },
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+    },
+  },
+}));
+
 export const Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: `posts/*.mdx`,
@@ -81,6 +134,7 @@ export default makeSource({
   contentDirPath: "content",
   documentTypes: [
     Post,
+    Leetcode,
     // Project
   ],
   mdx: {
